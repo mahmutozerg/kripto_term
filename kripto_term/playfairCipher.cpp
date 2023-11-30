be3631b590e64fde6225fa7b881a7827ea21c525
Çağatay <103611008+NarielV@users.noreply.github.com>
2023-11-30 16:51:02 +0300

    Added Playfair Cipher
    No conversion to binary yet.

---
+++ kripto_term/playfairCipher.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Playfair.h"
+
+#define SIZE 30
+
+using namespace std;
+
+
+
+// Driver code
+int main()
+{
+	char str[SIZE], key[SIZE];
+
+	// Key to be encrypted
+	strcpy(key, "Monarchy");
+	cout << "Key Text: " << key << endl;
+
+    // Plain text
+    strcpy(str, "instruments");
+    cout << "Plain text: " << str << "\n";
+ 
+    // Encrypt using Playfair Cipher
+    encryptByPlayfairCipher(str, key);
+    cout << "Cipher text: " << str << "\n";
+
+    // Print each character's ASCII values
+    vector<int> asciiValues = cipherTextToAsciiValues(str);
+    printAsciiValues(asciiValues);
+
+	return 0;
+}
