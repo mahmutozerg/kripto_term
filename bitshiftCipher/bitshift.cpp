1966c507d5249b7f3710f16a65bd5d7bb85fe04a
Çağatay <103611008+NarielV@users.noreply.github.com>
2023-12-04 11:04:11 +0300

    Updated bitshift.cpp
    Removed debug lines.

--- bitshiftCipher/bitshift.cpp
+++ bitshiftCipher/bitshift.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cstring> 
 #include <vector>
 #include <algorithm> 
 
@@ -17,15 +16,12 @@ vector<int> bitshiftCipher(const char* message, const char* key) {
     // Encryption happens inside this loop.
     for (int i = 0; i < msgLen; i++) {
         int msgAscii = static_cast<unsigned char>(message[i]); // Get the ASCII value of the message character.
-        cout << "Debug >> Message ASCII: " << msgAscii << endl;
 
         // Perform encryption using the key.
         for (int k = 0; k < keyLen; k++) {
             int keyAscii = static_cast<unsigned char>(mutableKey[k]); // Get the ASCII value of the key character.
-            cout << "Debug >> Key ASCII: " << keyAscii << endl;
-            cout << "Debug >> Key char: " << mutableKey[k] << endl;
 
-            // x = x + 3 << key % 8
+            // x = x + 9 << i % 5
             msgAscii = (msgAscii + 9) << (keyAscii % 5);
         }
 
